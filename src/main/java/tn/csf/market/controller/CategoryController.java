010f3c89fc7e8bdb4714a5b949f6bad00abf5799
Abir-Affes <abir.affes@ensi-uma.tn>
2023-06-18 16:24:04 +0200

    commit 5

--- src/main/java/tn/csf/market/controller/CategoryController.java
+++ src/main/java/tn/csf/market/controller/CategoryController.java
@@ -2,6 +2,7 @@ package tn.csf.market.controller;
 
 import java.util.List;  
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;  
 import org.springframework.web.bind.annotation.GetMapping;  
 import org.springframework.web.bind.annotation.PathVariable;  
@@ -15,7 +16,7 @@ import tn.csf.market.service.CategoryService;
 
 //mark class as Controller  
 @RestController 
-//@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 
 public class CategoryController {
 
